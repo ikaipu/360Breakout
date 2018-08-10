48b5801366066d1c569d98e8e96aeae68055ad19
zapicojunredexter <zapicojunredexter@yahoo.com>
2018-08-11 00:31:32 +0800

    add script for bounciing

--- Assets/BounceScript.cs
+++ Assets/BounceScript.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class BounceScript : MonoBehaviour {
 
+	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
+		rb.isKinematic = false;
+		rb.AddForce(new Vector3(100f,100f,0));
 	}
 	
 	// Update is called once per frame
