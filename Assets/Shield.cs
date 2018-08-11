c6a37c53e04a19d1019b7289a1fa0c927342159b
ikaipu <kabikabitznz@gmail.com>
2018-08-11 15:47:28 +0800

    sheld follows stick

---
+++ Assets/Shield.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.XR;
+ 
+ public class Shield : MonoBehaviour {
+ 
+     // Use this for initialization
+     void Start () {
+ 
+     }
+ 
+     // Update is called once per frame
+     void Update () {
+         transform.position = (InputTracking.GetLocalPosition(XRNode.RightHand) - InputTracking.GetLocalPosition(XRNode.Head)) * 15 + new Vector3(-3, 3, 2);  
+         transform.eulerAngles = InputTracking.GetLocalRotation(XRNode.RightHand).eulerAngles;
+     }
+ }
