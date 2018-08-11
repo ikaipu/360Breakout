bb16bbcb8ff1fa120fcd45c9421ba9049ad231e1
zapicojunredexter <zapicojunredexter@yahoo.com>
2018-08-11 16:55:25 +0800

    shield now has collider

---
+++ Assets/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody rb;
+	void Start () {
+		rb=GetComponent<Rigidbody>();
+		rb.isKinematic = false;
+		rb.AddForce(new Vector3(0,50f,-50f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
