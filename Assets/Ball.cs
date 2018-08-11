6803ce64a1b912a0ccbb8d987c59ae1829fb916a
zapicojunredexter <zapicojunredexter@yahoo.com>
2018-08-11 17:37:41 +0800

    has bouncing balls

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour {
 
 	// Use this for initialization
 	Rigidbody rb;
+	
+	public Text text;
+
+	float currCountdownValue;
+		public IEnumerator StartCountdown(float countdownValue = 10)
+		{
+				currCountdownValue = countdownValue;
+				while (currCountdownValue > 0)
+				{
+						Debug.Log("Countdown: " + currCountdownValue);
+
+						text.text = "Countdown: " + currCountdownValue;	
+						yield return new WaitForSeconds(1.0f);
+						currCountdownValue--;
+				}
+		}
 	void Start () {
+
+		text.text = "gina edit na";	
+		Debug.Log("start ball");
 		rb=GetComponent<Rigidbody>();
 		rb.isKinematic = false;
 		rb.AddForce(new Vector3(0,50f,-50f));
+
+		
+		 StartCoroutine(StartCountdown());
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	}
+
+	void OnCollisioEnter(Collision col){
+		Debug.Log("nicolllide ball");
 	}
 }
---
+++ Assets/Human.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisioEnter(Collision col){
+		Debug.Log("nicollide box");
+	}
+}
